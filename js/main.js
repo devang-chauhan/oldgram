c9aa2dd60a6bf5ef10f7c1ec35bf3f04351dc314
Devang <devngc@gmail.com>
2022-09-18 15:13:33 -0400

    fix(main.js): basic swipe capability added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const nameElArr = Array.from(document.getElementsByClassName("name"));
 const locationEl = document.getElementById("location");
 const postEl = document.getElementById("post");
 const likesEl = document.getElementById("likes");
-const user = posts[0];
+let count = 0;
+const user = posts[count];
 
 
 function update(user) {
@@ -26,3 +27,48 @@ function update(user) {
 }
 
 update(user);
+
+
+function handleTouch(start,end, left, right){
+  const xDist = end - start;
+  console.log(`Horizontal distance is ${xDist}`);
+   if(xDist > 100){
+      right();
+    }else if(xDist<(100*-1)){
+      left();
+    }
+}
+
+
+var left = () => {
+    count++;
+    if (count > posts.length - 1) {
+        count = 2;
+    }
+    const user = posts[count];
+    update(user);
+    
+};
+var right = () => {
+    count--;
+    if (count < 0) {
+        count = 0;
+    }
+    const user = posts[count];
+    update(user);
+};
+
+
+window.onload = function () {
+    let startX = 0;
+    let endX = 0;
+    window.addEventListener('touchstart', function (event) {
+        startX = event.touches[0].clientX;
+    });
+  
+    window.addEventListener('touchend', function (event) {
+        endX = event.changedTouches[0].clientX;
+        handleTouch(startX, endX, left, right);
+   
+    });
+};
